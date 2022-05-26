2248680b0c5765c454e92ae0bd7d16ffabadadbf
KarimMoustamid <105139320+KarimMoustamid@users.noreply.github.com>
2022-05-26 18:45:46 -0500

    more annotations

--- 02-annotations/variables.ts
+++ 02-annotations/variables.ts
@@ -31,3 +31,33 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When ti use annotations :
+// 1) Function that returns the "any" type .
+
+const json = '{x} : 10 , {y} : 20';
+
+const coordinates: { x: number; y: number } = JSON.parse(json);
+
+// 2) When we declare a variaable on one line
+// and initialize it later .
+
+let words = ['red', 'green', 'blue'];
+let founfword;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    founfword = true;
+  }
+}
+
+// 3) When a Variable whose type cannot be inferred correctly .
+
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
