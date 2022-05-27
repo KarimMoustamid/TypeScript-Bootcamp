18d9912fa11270520f2835207fcf1005fa7173cb
KarimMoustamid <105139320+KarimMoustamid@users.noreply.github.com>
2022-05-27 13:25:46 -0500

    tuples

---
+++ 02-annotations/tuples.ts
@@ -0,0 +1,15 @@
+const drink = {
+  color: 'green',
+  carbonated: true,
+  sugar: 40,
+};
+
+type Drink = [string, boolean, number];
+const pepsi: Drink = ['green', true, 40];
+
+const carSpec: [number, number] = [400, 3352];
+
+const carStast = {
+  horsepower: 400,
+  weight: 3354,
+};
